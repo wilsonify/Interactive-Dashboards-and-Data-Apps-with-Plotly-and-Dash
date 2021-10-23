9d5356817f840b911ac6ef9604fea1ff1bcefb35
Tom Wilson <tom.andrew.wilson@gmail.com>
2021-10-23 14:52:43 -0400

    save tests

--- tests/test_parquet_table/test_parquet_table_app.py
+++ tests/test_parquet_table/test_parquet_table_app.py
@@ -1,3 +1,6 @@
+import site
+import os
+import sysconfig
 from parquet_table.app import path_to_this_file, path_to_this_folder, path_to_data
 
 
@@ -9,3 +12,9 @@ def test_path_to_this_file():
     assert path_to_this_file == "/home/thom/repos/Interactive-Dashboards-and-Data-Apps-with-Plotly-and-Dash/parquet-table/parquet_table/app.py"
     assert path_to_this_folder == "/home/thom/repos/Interactive-Dashboards-and-Data-Apps-with-Plotly-and-Dash/parquet-table/parquet_table"
     assert path_to_data == "/home/thom/repos/Interactive-Dashboards-and-Data-Apps-with-Plotly-and-Dash/parquet-table/data"
+
+
+def test_site_packages():
+    result = site.getsitepackages()[0]
+    result_head, result_tail = os.path.split(result)
+    assert result_tail == "site-packages"
