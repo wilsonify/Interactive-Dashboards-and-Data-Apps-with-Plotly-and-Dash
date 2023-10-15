47aa6e630350f92fd1ffe8652b23c4d5e540c4d1
wilsonify <tom.andrew.wilson@gmail.com>
2023-10-15 13:14:38 -0400

    minimal react app.js

--- 01-overview-of-dash-ecosystem/v1_4_ts/src/App.js
+++ 01-overview-of-dash-ecosystem/v1_4_ts/src/App.js
@@ -1,14 +1,35 @@
-import './App.css';
-import Discovery from './components/Discovery';
-import DataFlow from './components/DataFlow';
+import React from 'react';
 
-const isDiscovery = window.location.pathname.replace(/\//g, '') === 'discovery';
-  
 function App() {
-  if (isDiscovery) {
-    return <Discovery />;
-  }
-  return <DataFlow />;
+  return (
+    <div>
+      <h1>Poverty And Equity Database</h1>
+      <h2>The World Bank</h2>
+      <div className="tabs">
+        <div className="tab">
+          <ul>
+            <br />
+            <li>Number of Economies: 170</li>
+            <li>Temporal Coverage: 1974 - 2019</li>
+            <li>Update Frequency: Quarterly</li>
+            <li>Last Updated: March 18, 2020</li>
+            <li>
+              Source: <a href="https://datacatalog.worldbank.org/dataset/poverty-and-equity-database">https://datacatalog.worldbank.org/dataset/poverty-and-equity-database</a>
+            </li>
+          </ul>
+        </div>
+        <div className="tab">
+          <ul>
+            <br />
+            <li>Book title: Interactive Dashboards and Data Apps with Plotly and Dash</li>
+            <li>
+              GitHub repo: <a href="https://github.com/PacktPublishing/Interactive-Dashboards-and-Data-Apps-with-Plotly-and-Dash">https://github.com/PacktPublishing/Interactive-Dashboards-and-Data-Apps-with-Plotly-and-Dash</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
