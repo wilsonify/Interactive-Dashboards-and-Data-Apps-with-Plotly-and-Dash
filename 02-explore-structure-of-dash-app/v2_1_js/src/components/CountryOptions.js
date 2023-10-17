5d8bb75d8fab06aa5a134b2b48ca86a01f52b12d
wilsonify <tom.andrew.wilson@gmail.com>
2023-10-17 08:19:14 -0400

    working test

--- 02-explore-structure-of-dash-app/v2_1_js/src/components/CountryOptions.js
+++ 02-explore-structure-of-dash-app/v2_1_js/src/components/CountryOptions.js
@@ -1,7 +1,6 @@
-export function computeCountryOptionsUnique(data) {
+function computeCountryOptionsUnique(data) {
   const uniqueCountries = Array.from(new Set(data['Country Name']));
   return uniqueCountries.map((country) => ({ label: country, value: country }));
 }
 
-
-
+module.exports = computeCountryOptionsUnique;
--- 02-explore-structure-of-dash-app/v2_1_js/tests/CountryOptions.test.js
+++ 02-explore-structure-of-dash-app/v2_1_js/tests/CountryOptions.test.js
@@ -1,13 +1,26 @@
 
-import { computeCountryOptionsUnique } from '../src/components/CountryOptions.js';
-
+const computeCountryOptionsUnique = require('../src/components/CountryOptions');
 
 
 describe('CountryOptions', () => {
   it('renders the expected options', async () => {
     // Assuming that `povertyData` is your JSON data
-    const povertyData = { 'Country Name': ['Afghanistan', 'Albania', 'Algeria', 'Angola', 'Argentina'] };
-    const countryOptionsUnique = computeCountryOptionsUnique(povertyData);
+    const povertyData = {
+     'Country Name': ['Afghanistan', 'Albania', 'Algeria', 'Angola', 'Argentina']
+     };
+    var countryOptionsUnique = computeCountryOptionsUnique(povertyData);
     console.log(countryOptionsUnique);
+
+    const expectedOptions = [
+      { label: 'Afghanistan', value: 'Afghanistan' },
+      { label: 'Albania', value: 'Albania' },
+      { label: 'Algeria', value: 'Algeria' },
+      { label: 'Angola', value: 'Angola' },
+      { label: 'Argentina', value: 'Argentina' },
+    ];
+
+    // Use Jest's expect function to make the assertion
+    expect(countryOptionsUnique).toEqual(expectedOptions);
+
   });
 });
